877d321148767b94d0d3c8a639971929a7caca25
r0ck3r008 <naman.arora@tutanota.com>
2018-06-15 02:06:07 +0530

    updated client authentication bindings as a fake_peer

--- peer.c
+++ peer.c
@@ -7,16 +7,31 @@
 #include<arpa/inet.h>
 #include<openssl/rsa.h>
 #include<openssl/pem.h>
+#include<openssl/err.h>
 #include<pthread.h>
 #include<errno.h>
 
-int server_sock, db_sock;
+struct client
+{
+    int sock;
+    int fellow;
+    struct sockaddr_in addr;
+    RSA *ku;
+    int id;
+};
+int server_sock, db_sock, cli_count=0;
 RSA *ku, *kv;
 
 //prototypes
 void *allocate(char *, int);
 int init(int);
 int server_init(char *);
+int server_workings();
+void *server_run(void *);
+void *cli_run(void *);
+int snd(int, char *, char *);
+char *rcv(int, char *);
+int connect_to_fellow_peer(struct client *);
 RSA *gen_keys(char *, int);
 int dbconnect(char *);
 int update_existance_in_peers(char *);
@@ -80,6 +95,152 @@ int server_init(char *argv1)
 
     printf("\n[!]listning on %s:%d...\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
     return s;
+}
+
+int server_workings()
+{
+    pthread_t server_thr;
+    int stat;
+    char *retval;
+
+    if((stat=pthread_create(&server_thr, NULL, server_run, "..."))!=0)
+    {
+        fprintf(stderr, "\n[-]Error in starting server thread: %s\n", strerror(stat));
+        return 1;
+    }
+
+    if((stat=pthread_join(server_thr, (void **)retval))!=0)
+    {
+        fprintf(stderr, "\n[-]Error in joining to server thread: %s\n", strerror(stat));
+        return 1;
+    }
+    printf("\n[!]Server thread returned with status %s\n", (char *)retval);
+}
+
+void *server_run(void *a)
+{
+    pthread_t tid[10];
+    struct client cli[10];
+    socklen_t len=sizeof(struct sockaddr_in);
+    int stat;
+    char *retval;
+
+    printf("\n[!]Server thread initiated...\n");
+
+    for(cli_count; cli_count<10; cli_count++)
+    {
+        if((cli[cli_count].sock=accept(server_sock, (struct sockaddr *)&cli[cli_count].addr, &len))<0)
+        {
+            fprintf(stderr, "\n[-]Error in accepting client %d: %s\n", cli_count, strerror(errno));
+            continue;
+        }
+
+        if((stat=pthread_create(&tid[cli_count], NULL, cli_run, &cli[cli_count]))!=0)
+        {
+            fprintf(stderr, "\n[-]Error in initiating client thread at %s:%d: %s\n", inet_ntoa(cli[cli_count].addr.sin_addr), ntohs(cli[cli_count].addr.sin_port), strerror(stat));
+            continue;
+        }
+    }
+
+    for(int i=0; i<cli_count; i++)
+    {
+        if((stat=pthread_join(tid[i], (void **)&retval))!=0)
+        {
+            fprintf(stderr, "\n[-]Error in joining to client %s:%d: %s\n", inet_ntoa(cli[i].addr.sin_addr), ntohs(cli[i].addr.sin_port), strerror(stat));
+            continue;
+        }
+        printf("\n[!]Client %s:%d returned %s\n", inet_ntoa(cli[i].addr.sin_addr), ntohs(cli[i].addr.sin_port), (char *)retval);
+    }
+
+    pthread_exit("SUCCESS");
+}
+
+void *cli_run(void *c)
+{
+    struct client cli=*(struct client *)c;
+    char *cmds=(char *)allocate("char", 2048), *cmdr;
+    int bit;
+
+    printf("\n[!]Now handelling client %s:%d\n", inet_ntoa(cli.addr.sin_addr), ntohs(cli.addr.sin_port));
+    
+    if((cmdr=rcv(cli.sock, "to receive first packet\n"))==NULL)
+    {
+        pthread_exit("ERROR IN RECEVING");
+    }
+    
+    bit=(int)strtol(strtok(cmdr, ":"), NULL, 10);
+    if(bit)
+    {
+        cli.fellow=1;
+    }
+    else
+    {
+        struct client fellow;
+        char *cmdr_en;
+        cli.fellow=0;
+        fellow.addr.sin_addr.s_addr=inet_addr(strtok(NULL, ":"));
+        fellow.addr.sin_port=htons(6666);
+        fellow.addr.sin_family=AF_INET;
+        cmdr_en=strtok(NULL, ":");
+
+        if(connect_to_fellow_peer(&fellow))
+        {
+            pthread_exit("ERROR IN CONNECTING TO FELLOW PEER");
+        }
+
+        if(snd(fellow.sock, cmdr_en, "send encrypted msg to fellow peer"))
+        {
+            pthread_exit("ERROR IN SENDING");
+        }
+    }
+
+    free(cmdr);
+    pthread_exit("SUCCESS");
+}
+
+int snd(int sock, char *cmds, char *reason)
+{
+    if(send(sock, cmds, sizeof(char)*2048, 0)<0)
+    {
+        fprintf(stderr, "\n[-]Error in sending for %s: %s\n", reason, strerror(errno));
+        return 1;
+    }
+
+    free(cmds);
+    return 0;
+}
+
+char *rcv(int sock, char *reason)
+{
+    char *cmdr=(char *)allocate("char", 2048);
+
+    if(recv(sock, cmdr, sizeof(char)*2048, 0)<0)
+    {
+        fprintf(stderr, "\n[-]Error in receving %s: %s\n", reason, strerror(errno));
+        return NULL;
+    }
+
+    return cmdr;
+}
+
+int connect_to_fellow_peer(struct client *fell)
+{
+    int s;
+
+    if((s=socket(AF_INET, SOCK_STREAM, 0))<0)
+    {
+        fprintf(stderr, "\n[-]Error in creating socket for fellow peer: %s\n", strerror(errno));
+        return 1;
+    }
+
+    if(connect(s, (struct sockaddr *)&(fell->addr), sizeof(fell->addr))<0)
+    {
+        fprintf(stderr, "\n[-]Error in connecting to fellow peer at %s: %s\n", inet_ntoa(fell->addr.sin_addr), strerror(errno));
+        return 1;
+    }
+
+    fell->sock=s;
+    return 0;
 }
 
 RSA *gen_keys(char *fname, int pub)
@@ -176,6 +337,11 @@ int main(int argc, char *argv[])
     }
 
     if((server_sock=server_init(argv[1]))==0)
+    {
+        _exit(-1);
+    }
+
+    if(server_workings())
     {
         _exit(-1);
     }
