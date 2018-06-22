863e932f98d7d1869bec012aa5ab917aa43041b7
r0ck3r008 <naman.arora@tutanota.com>
2018-06-22 21:27:00 +0530

    query db function source and header updated with client bindings

---
+++ dbinterface.d/query_db.c
@@ -0,0 +1,42 @@
+#define NEEDS_ALL
+
+#include"query_db.h"
+#include"allocate.h"
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+
+int query_db(struct joinee *j, char *query, char *cmds, int exp_col)
+{
+    int stat;
+    explicit_bzero(res, sizeof(char)*2048);
+
+    if((stat=pthread_mutex_lock(&mutex))!=0)
+    {
+        fprintf(stderr, "\n[-]Error in locking mutex for %s:%d: %s\n", inet_ntoa(j->addr.sin_addr), ntohs(j->addr.sin_port), strerror(stat));
+        return 1;
+    }
+
+    if(mysql_query(conn, query))
+    {
+        fprintf(stderr, "\n[-]Error in querying %s for %s:%d: %s\n", query, inet_ntoa(j->addr.sin_addr), ntohs(j->addr.sin_port), mysql_error(conn));
+        return 1;
+    }
+
+    res=mysql_use_result(conn);
+    row=mysql_fetch_row(res);
+    for(int i=0; i<exp_col; i++)
+    {
+        strcat(cmds, row[i]);
+    }
+    mysql_free_result(res);
+
+    if((stat=pthread_mutex_unlock(&mutex))!=0)
+    {
+        fprintf(stderr, "\n[-]Error in unlocking mutex for %s query for %s:%d: %s\n", query, inet_ntoa(j->addr.sin_addr), ntohs(j->addr.sin_port), strerror(stat));
+        return 1;
+    }
+
+    sprintf(cmds, "SUCCESS:%s", cmds);
+    return 0;
+}
---
+++ dbinterface.d/query_db.h
@@ -0,0 +1,8 @@
+#ifndef QUERY_DB_H
+#define QUERY_DB_H
+
+#include"global_defs.h"
+
+int query_db(struct joinee *, char *, char *, int);
+
+#endif
