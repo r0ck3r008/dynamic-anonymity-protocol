2a79cb0c9c0b1d65b3a028a87a520e64d211dd68
r0ck3r008 <naman.arora@tutanota.com>
2018-06-22 21:25:53 +0530

    global definitions

---
+++ dbinterface.d/global_defs.h
@@ -0,0 +1,36 @@
+#ifndef GLOBAL_DEFS_H   //main
+#define GLOBAL_DEFS_H
+
+    #ifdef NEEDS_ALL
+        #define NEEDS_DB_GLOBALS
+        #define NEEDS_MUTEX_GLOBALS
+        #define NEEDS_JOINEE_GLOBALS
+    #endif
+
+    #ifdef NEEDS_DB_GLOBALS     //for db
+        #include<mysql/mysql.h>
+        MYSQL *conn;
+        MYSQL_RES *res;
+        MYSQL_ROW row;
+    #endif      //for db end
+
+int server_psock, server_csock;
+int cli_count, peer_count;
+
+    #ifdef NEEDS_MUTEX_GLOBALS  //for mutex
+        #include<pthread.h>
+        pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+    #endif      //for mutex end
+
+    #ifdef NEEDS_JOINEE_GLOBALS     //for joinee related
+        #include<sys/socket.h>
+        #include<netinet/in.h>
+        #include<arpa/inet.h>
+        struct joinee
+        {
+            int sock, peer;
+            struct sockaddr_in addr;
+        };
+    #endif      //for joinee end
+
+#endif      //main end
