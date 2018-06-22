78437d4069b01e8eb57235d753a999992c27046b
r0ck3r008 <naman.arora@tutanota.com>
2018-06-22 21:22:04 +0530

    get_connect_to_new_peer souce and header

---
+++ client.d/get_connect_to_new_peer.c
@@ -0,0 +1,27 @@
+#define NEEDS_STRUCT_GLOBALS
+
+#include"get_connect_to_new_peer.h"
+#include"get_rand_sno.h"
+#include"get_rand_peer.h"
+#include"connect_to_new_peer.h"
+
+struct peer_combo get_connect_to_new_peer(int *a, char *ku_fname)
+{
+    struct peer_combo pc;
+    if((pc.rand_sno=get_rand_sno())==-1)
+    {
+        *a=1;
+    }
+
+    if(get_rand_peer(&pc.p, pc.rand_sno, ku_fname))
+    {
+        *a=1;
+    }
+
+    if(connect_to_new_peer(&pc.p))
+    {
+        *a=1;
+    }
+
+    return pc;
+}
---
+++ client.d/get_connect_to_new_peer.h
@@ -0,0 +1,7 @@
+#ifndef GET_CONNECT_TO_NEW_PEER_H
+#define GET_CONNECT_TO_NEW_PEER_H
+
+#include"global_defs.h"
+struct peer_combo get_connect_to_new_peer(int *, char *);
+
+#endif
