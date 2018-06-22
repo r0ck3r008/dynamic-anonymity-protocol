db25b7e9a92b7654f5a96274238d8bfb98423e06
r0ck3r008 <naman.arora@tutanota.com>
2018-06-22 21:14:43 +0530

    gen keys function header and source

--- client.d/gen_keys.c
+++ client.d/gen_keys.c
@@ -1,18 +1,18 @@
 #include"gen_keys.h"
 #include<stdio.h>
+#include<string.h>
 #include<openssl/pem.h>
 #include<openssl/err.h>
-#include<string.h>
 #include<errno.h>
 
 RSA *gen_keys(char *fname, int pub)
 {
     FILE *f;
-    RSA *rsa=RSA_new();
+    RSA *rsa= RSA_new();
 
     if((f=fopen(fname, "rb"))==NULL)
     {
-        fprintf(stderr, "\n[-]Error in opening %s: %s\n", fname, strerror(errno));
+        fprintf(stderr, "\n[-]Error in opening file %s: %s\n", fname, strerror(errno));
         return NULL;
     }
 
@@ -25,6 +25,6 @@ RSA *gen_keys(char *fname, int pub)
         rsa=PEM_read_RSAPrivateKey(f, &rsa, NULL, NULL);
     }
 
+    fclose(f);
     return rsa;
 }
-
