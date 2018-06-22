f5b63138ee79ac24eab4cd44f505adbed965f8f6
r0ck3r008 <naman.arora@tutanota.com>
2018-06-22 21:24:42 +0530

    get_passwd function source and header

--- dbinterface.d/get_passwd.c
+++ dbinterface.d/get_passwd.c
@@ -1,20 +1,22 @@
 #include"get_passwd.h"
 #include"allocate.h"
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
 #include<termios.h>
 #include<errno.h>
 
 char *get_passwd()
 {
-    char *passwd=(char *)allocate("char", 50);
+    char *passwd=(char *)allocate("char", 64);
     struct termios a, b;
     explicit_bzero(&a, sizeof(a));
     explicit_bzero(&b, sizeof(b));
 
-    if(tcgetattr(fileno(stdin), &a)<0)
+    if(tcgetattr(fileno(stdin), &a)==-1)
     {
-        fprintf(stderr, "\n[-]Error in getting attributes: %s\n", strerror(errno));
+        fprintf(stderr, "\n[-]Error in getting stdin attr: %s\n", strerror(errno));
         return NULL;
     }
 
@@ -22,21 +24,19 @@ char *get_passwd()
     a.c_lflag &= ~ECHO;
     a.c_lflag |= ECHONL;
 
-    if(tcsetattr(fileno(stdin), TCSANOW, &a)<0)
+    if(tcsetattr(fileno(stdin), TCSANOW, &a)==-1)
     {
-        fprintf(stderr, "\n[-]Error in setting no echo flag :%s\n", strerror(errno));
+        fprintf(stderr, "\n[-]Error in setting quite flag in stdin: %s\n", strerror(errno));
         return NULL;
     }
 
-    fgets(passwd, sizeof(char)*50, stdin);
+    fgets(passwd, sizeof(char)*64, stdin);
 
-    if(tcsetattr(fileno(stdin), TCSANOW, &b)<0)
+    if(tcsetattr(fileno(stdin), TCSANOW, &b)==-1)
     {
-        fprintf(stderr, "\n[-]Error in makin stdin normal: %s\n", strerror(errno));
+        fprintf(stderr, "\n[-]Error in normalising stdin back: %s\n", strerror(errno));
         return NULL;
     }
 
     return passwd;
 }
-
-
