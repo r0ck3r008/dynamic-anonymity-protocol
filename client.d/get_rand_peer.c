c4cc4abac4c2758f4f6195e73c630763fe35235f
r0ck3r008 <naman.arora@tutanota.com>
2018-06-22 21:15:24 +0530

    get_random_peer function source and header

--- client.d/get_rand_peer.c
+++ client.d/get_rand_peer.c
@@ -1,71 +1,61 @@
+#define NEEDS_STRUCT_GLOBALS
+
 #include"get_rand_peer.h"
 #include"allocate.h"
 #include"snd_rcv.h"
-#include"dbconnect.h"
-#include"gen_keys.h"
+#include"write_key_to_file.h"
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<sodium.h>
-#include<errno.h>
+#include<stdlib.h>
 
-int get_rand_peer(struct peer *p, int rand_sno, char *ku_fname) //ku_fname is not null while selecting const_peer
+int get_rand_peer(struct peer *p, int rand_sno, char *ku_fname)
 {
-    char *query=(char *)allocate("char", 2048), *cmdr, *ip=(char *)allocate("char", 20);
+    char *cmds=(char *)allocate("char", 2048), *cmdr;
+    char *ip=(char *)allocate("char", 20);
 
+    //segregate if its const_peer or fake_peers
     if(ku_fname!=NULL)
     {
-        sprintf(query, "0:1:3:select id, ip, ku from peers where sno=%d;", rand_sno);
+         sprintf(cmds, "3:select id, ip, ku from peers where sno=%d;", rand_sno);
     }
     else
     {
-        sprintf(query, "0:1:2:select id, ip from peers where sno=%d;", rand_sno);
+         sprintf(cmds, "2:select id, ip from peers where sno=%d", rand_sno);
     }
 
-    if(snd(db_sock, query, "get ip, id, and key from db_inteface\n"))
+    //send query
+    if(snd(db_sock, cmds, "send query to get random peer"))
     {
         return 1;
     }
 
-    if((cmdr=rcv(db_sock, "receive id, ip, key from peers via rand_sno\n"))==NULL)
+    if((cmdr=rcv(db_sock, "to receive random peer query resp"))==NULL)
     {
         return 1;
     }
 
-    p->id=(int)strtol(strtok(cmdr, ":"), NULL, 10);
+    //check if success
+    if(strcmp(strtok(cmdr, ":"), "SUCCESS")!=0)
+    {
+        fprintf(stderr, "\n[-]dbinterface returned %s for rand_peer query for rand_sno %d\n", strtok(cmdr, ":"), rand_sno);
+        return 1;
+    }
+    //fill struct peer *p
+    p->id=(int)strtol(strtok(NULL, ":"), NULL, 10);
     sprintf(ip, "%s", strtok(NULL, ":"));
     p->addr.sin_addr.s_addr=inet_addr(ip);
     p->addr.sin_port=htons(6666);
-    p->ku_fname=ku_fname;
+    p->addr.sin_family=AF_INET;
 
     if(ku_fname!=NULL)
     {
-        char *key_str=strtok(NULL, ":");
-        FILE *f;
-        if((f=fopen(ku_fname, "w"))==NULL)
-        {
-            fprintf(stderr, "\n[-]Error in opening file %s: %s\n", ku_fname, strerror(errno));
-            return 1;
-        }
-        for(int i=0; i<strlen(key_str); i++)
-        {
-            fprintf(f, "%c", key_str[i]);
-        }
-        fclose(f);
-        if((p->ku=gen_keys(ku_fname, 1))==NULL)
+        p->ku_fname=ku_fname;
+        if((p->ku=write_key_to_file(ku_fname, strtok(NULL, ":")))==NULL)
         {
             return 1;
         }
-    }
-    else
-    {
-        p->ku=NULL;
     }
 
     free(cmdr);
     return 0;
 }
-
