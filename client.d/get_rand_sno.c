8e4314576ed61d3d4e739a33a07cc02d2d4ffe30
r0ck3r008 <naman.arora@tutanota.com>
2018-06-22 21:15:57 +0530

    get_random_sno functio source and header

--- client.d/get_rand_sno.c
+++ client.d/get_rand_sno.c
@@ -1,31 +1,35 @@
 #include"get_rand_sno.h"
 #include"allocate.h"
 #include"snd_rcv.h"
-#include"dbconnect.h"
+#include"global_defs.h"
+#include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<sodium.h>
 
 int get_rand_sno()
 {
-    char *query=(char *)allocate("char", 2048);
-    char *cmdr;
-    sprintf(query, "0:1:1:select count(*) from peers;");    //0 for non-insert, 1 for using peers table, 1 for 1 exp_col
+    char *cmds=(char *)allocate("char", 2048), *cmdr;
+    int rand_sno;
     
-    if(snd(db_sock, query, "query to count number of peers"))
+    sprintf(cmds, "1:select count(*) from peers;");
+    if(snd(db_sock, cmds, "send a query to get total numbers of peers"))
     {
         return -1;
     }
 
-    if((cmdr=rcv(db_sock, "to receive the number of peers from dbinterface"))==NULL)
+    if((cmdr=rcv(db_sock, "receibe total number of peers"))==NULL)
     {
         return -1;
     }
 
-    int peer_count=(int)strtol(cmdr, NULL, 10);
-    int rand_sno=(int)randombytes_uniform(peer_count);
+    if(strcmp(strtok(cmdr, ":"), "SUCCESS")!=0)
+    {
+        fprintf(stderr, "\n[-]Dbinterface returned %s\n", strtok(cmdr, ":"));
+        return -1;
+    }
+    rand_sno=(int)randombytes_uniform((int)strtol(strtok(NULL, ":"), NULL, 10));
 
     free(cmdr);
     return rand_sno;
 }
-
--- client.d/get_rand_sno.h
+++ client.d/get_rand_sno.h
@@ -1,23 +1,6 @@
 #ifndef GET_RAND_SNO_H
 #define GET_RAND_SNO_H
 
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<openssl/rsa.h>
-struct peer
-{
-    int sock, id;
-    struct sockaddr_in addr;
-    RSA *ku;
-    char *ku_fname;
-} dest_peer;
-struct peer_combo
-{
-    struct peer p;
-    int rand_sno;
-} const_peer;
-
 int get_rand_sno();
 
 #endif
