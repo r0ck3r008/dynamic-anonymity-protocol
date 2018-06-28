14eb9e684685f1672495dc598ec6ec98e4c11d21
r0ck3r008 <naman.arora@tutanota.com>
2018-06-29 03:57:14 +0530

    cut down joinee and added new clients

--- peer.d/global_defs.h
+++ peer.d/global_defs.h
@@ -3,9 +3,13 @@
 
 int db_sock, server_psock, server_csock;
 int my_sno, my_rand_id;
+int peer_count, cli_count;
+int max_clients;
 
     #ifdef NEEDS_ALL
         #define NEEDS_KEYS
+        #define NEEDS_JOINEE_STRUCT
+        #define NEEDS_MUTEX
     #endif
 
     #ifdef NEEDS_KEYS
@@ -13,4 +17,30 @@ int my_sno, my_rand_id;
         RSA *ku, *kv;
     #endif
 
+    #ifdef NEEDS_JOINEE_STRUCT
+        #include<sys/socket.h>
+        #include<netinet/in.h>
+        #include<arpa/inet.h>
+        #include<openssl/rsa.h>
+        struct client
+        {
+            int final_server_sock;
+            int cli_rand_num;
+            struct sockaddr_in final_server_addr;
+            char *ku_fname;
+            RSA *ku;
+        } *clients;
+        struct joinee
+        {
+            int sock, id;
+            struct sockaddr_in addr;
+            struct client *my_curr_client;
+        };
+    #endif
+
+    #ifdef NEEDS_MUTEX
+        #include<pthread.h>
+        extern pthread_mutex_t db_sock_mutex;
+        extern pthread_mutex_t max_clients_mutex;
+    #endif
 #endif
