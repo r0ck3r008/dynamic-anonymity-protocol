b76e2bd5922bb3d6d4e3177240099c5703abf292
r0ck3r008 <naman.arora@tutanota.com>
2018-06-29 04:02:22 +0530

    added new functonality to check regex'

---
+++ peer.d/regex_check.c
@@ -0,0 +1,28 @@
+#include"regex_check.h"
+#include"allocate.h"
+#include<stdio.h>
+#include<regex.h>
+#include<sys/types.h>
+#include<string.h>
+
+int regex_check(char *haystack, char *needle)
+{
+    regex_t regex;
+    int stat;
+    char *retval=(char *)allocate("char", 128);
+
+    if((stat=regcomp(&regex, needle, 0))!=0)
+    {
+        fprintf(stderr, "\n[-]%s\n", strerror(stat));
+        return 0;
+    }
+
+    if((stat=regexec(&regex, haystack, 0, NULL, 0))!=0)
+    {
+        fprintf(stderr, "\n[-]No match\n", strerror(stat));;
+        return 0;
+    }
+
+    regfree(&regex);
+    return 1;
+}
---
+++ peer.d/regex_check.h
@@ -0,0 +1,6 @@
+#ifndef REGEX_CHECK_H
+#define REGEX_CHECK_H
+
+int regex_check(char *, char *);
+
+#endif
