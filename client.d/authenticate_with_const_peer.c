2fded7097d0cbbc8bbdeaedebfd9a1c4c5a82203
r0ck3r008 <naman.arora@tutanota.com>
2018-06-29 03:38:02 +0530

    source and header for function to authenticate with const peer

---
+++ client.d/authenticate_with_const_peer.c
@@ -0,0 +1,41 @@
+#define NEEDS_ALL
+#define _GNU_SOURCE
+
+#include"authenticate_with_const_peer.h"
+#include"authenticate_with_tmp_peer.h"
+#include"global_defs.h"
+#include"allocate.h"
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sodium.h>
+#include<openssl/err.h>
+#include<errno.h>
+
+int authenticate_with_const_peer(char *ku_fname)
+{
+    pkt_num=0;
+    char *key=(char *)allocate("char", 2048);
+    FILE *f;
+    my_rand_num=(int)randombytes_uniform(1000000);
+
+    if((f=fopen(ku_fname, "r"))==NULL)
+    {
+        fprintf(stderr, "\n[-]Error in opening file %s: %s\n", ku_fname, strerror(errno));
+        return 1;
+    }
+    for(int i=0; !feof(f); i++)
+    {
+        fscanf(f, "%c", &key[i]);
+    }
+    sprintf(key, "%d:%s:%s", pkt_num++, server_ip, key); 
+
+    if(authenticate_with_tmp_peer(key))
+    {
+        return 1;
+    }
+
+    fclose(f);
+    free(key);
+    return 0;
+}
---
+++ client.d/authenticate_with_const_peer.h
@@ -0,0 +1,6 @@
+#ifndef AUTHENTICATE_WITH_CONST_PEER_H
+#define AUTHENTICATE_WITH_CONST_PEER_H
+
+int authenticate_with_const_peer(char *);
+
+#endif
