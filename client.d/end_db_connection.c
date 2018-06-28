0fbd1960c7ff617f6f746519f3061da833a3680a
r0ck3r008 <naman.arora@tutanota.com>
2018-06-29 03:40:23 +0530

    end db connection function source and header

---
+++ client.d/end_db_connection.c
@@ -0,0 +1,30 @@
+#include"end_db_connection.h"
+#include"global_defs.h"
+#include"allocate.h"
+#include"snd_rcv.h"
+#include<stdio.h>
+#include<string.h>
+
+int end_db_connection()
+{
+    char *cmds=(char *)allocate("char", 2048), *cmdr;
+
+    sprintf(cmds, "END");
+    if(snd(db_sock, cmds, "END DB CONNECTION"))
+    {
+        return 1;
+    }
+
+    if((cmdr=rcv(db_sock, "RECV ACK FOR DB CXN END"))==NULL)
+    {
+        return 1;
+    }
+
+    if(strcmp(cmdr, "ENDING DB CONNECTION")!=0)
+    {
+        fprintf(stderr, "\n[-]Error in ending db connection\n");
+        return 1;
+    }
+
+    return 0;
+}
---
+++ client.d/end_db_connection.h
@@ -0,0 +1,5 @@
+#ifndef END_DB_CONNECTION_H
+
+int end_db_connection();
+
+#endif
