37466e606e9f7c003b9c9d196ad0c84e5a244025
r0ck3r008 <naman.arora@tutanota.com>
2018-06-29 03:38:34 +0530

    source and header for function to authenticate with tmp peer

---
+++ client.d/authenticate_with_tmp_peer.c
@@ -0,0 +1,30 @@
+#define NEEDS_STRUCT_GLOBALS
+
+#include"authenticate_with_tmp_peer.h"
+#include"get_connect_to_new_peer.h"
+#include"allocate.h"
+#include"global_defs.h"
+#include"snd_rcv.h"
+#include<openssl/err.h>
+
+int authenticate_with_tmp_peer(char *cmds)
+{
+    int stat=0;
+    char *cmds_en1=(char *)allocate("char", 2048), *cmds_en=(char *)allocate("char", 2048);
+    tmp_peer=get_connect_to_new_peer(&stat, NULL);
+    if(RSA_public_encrypt(RSA_size(const_peer.p.ku)-11, cmds, cmds_en1, const_peer.p.ku, RSA_PKCS1_PADDING)<0)
+    {
+        fprintf(stderr, "\n[-]Error in encrypting %s to authenticate with tmp_peer: %s\n", ERR_get_error());
+        return 1;
+    }
+
+    sprintf(cmds_en, "%s:%d:%s", inet_ntoa(const_peer.p.addr.sin_addr), my_rand_num, cmds_en1);
+    if(snd(tmp_peer.p.sock, cmds_en, "send to tmp_peer for auth"))
+    {
+        return 1;
+    }
+    
+    free(cmds_en1);
+    free(cmds);
+    return 0;
+}
---
+++ client.d/authenticate_with_tmp_peer.h
@@ -0,0 +1,6 @@
+#ifndef AUTHENTICATE_WITH_TMP_PEER_H
+#define AUTHENTICATE_WITH_TMP_PEER_H
+
+int authenticate_with_tmp_peer(char *);
+
+#endif
