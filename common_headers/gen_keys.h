f80e4c0a5989236989948b9e628e20f7ee4639f7
r0ck3r008 <naman.arora@tutanota.com>
2018-07-19 21:53:28 +0530

    common sources and headers used by various modules clubbed together

---
+++ common_headers/gen_keys.h
@@ -0,0 +1,7 @@
+#ifndef GEN_KEYS_H
+#define GEN_KEYS_H
+
+#include<openssl/rsa.h>
+RSA *gen_keys(char *, int);
+
+#endif
---
+++ common_sources/snd_rcv.c
@@ -0,0 +1,29 @@
+#include"common_headers/snd_rcv.h"
+#include"common_headers/allocate.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<errno.h>
+
+int snd(int sock, char *cmds, char *reason)
+{
+    if(send(sock, cmds, 2048*sizeof(char), 0)<0)
+    {
+        fprintf(stderr, "\n[-]Error in sending %s for reason %s:%s\n", cmds, reason, strerror(errno));
+        return 1;
+    }
+
+    free(cmds);
+}
+
+char *rcv(int sock, char *reason)
+{
+    char *cmdr=(char *)allocate("char", 2048);
+
+    if(recv(sock, cmdr, sizeof(char)*2048, 0)<0)
+    {
+        fprintf(stderr, "\n[-]Error in receving for reason %s:%s\n", reason, strerror(errno));
+        return NULL;
+    }
+}
